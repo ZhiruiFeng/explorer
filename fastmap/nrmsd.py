ad6d88667b02daec102f909892b4763a7992e4a7
ZhiruiFeng <fihezro@gmail.com>
2018-05-22 13:30:02 -0700

    modify nrmsd

--- fastmap/nrmsd.py
+++ fastmap/nrmsd.py
@@ -14,7 +14,7 @@ def nrmsd(G, embedding, S, alg = 'L1'):
         node_1 = subset[i]
         emb_1 = np.array(embedding[node_1])
         length = nx.single_source_dijkstra_path_length(G, node_1)
-        for j in range(i+1, S):
+        for j in range(S):
             node_2 = subset[j]
             emb_2 = np.array(embedding[node_2])
             distance = length[node_2]
@@ -24,7 +24,7 @@ def nrmsd(G, embedding, S, alg = 'L1'):
                 embdis = math.sqrt(np.dot(emb_1-emb_2, emb_1-emb_2))
             sigma += (distance-embdis)*(distance-embdis)
             ave_d += distance
-    base = S*(S-1)/2
+    base = S*S
     sigma = math.sqrt(float(sigma)/base)
     ave_d = float(ave_d)/base
     return float(sigma)/ave_d
