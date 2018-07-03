a33016c366cc75a35710b9efb1177e6aa83349b1
ZhiruiFeng <fihezro@gmail.com>
2018-07-02 23:41:21 -0700

    properties

--- heuristic/fastmap/fastmap.py
+++ heuristic/fastmap/fastmap.py
@@ -26,6 +26,7 @@ def fastmap_L1(G, K, epsilon):
         length_a = nx.single_source_dijkstra_path_length(NG, node_a)
         length_b = nx.single_source_dijkstra_path_length(NG, node_b)
         dis_ab = length_a[node_b]
+        print("Iteration {}, largest distance: {}".format(r+1, dis_ab))
         if dis_ab < epsilon:
             break
         # Calcute the embedding
@@ -38,7 +39,6 @@ def fastmap_L1(G, K, epsilon):
             NG[i][j]['weight'] = w - abs(embedding[i][r]-embedding[j][r])
             if abs(NG[i][j]['weight'])<0.00001:
                 NG[i][j]['weight'] = 0
-    print("The final dimension of embedding: {}".format(r+1))
     return embedding
 
 def adjust_length(raw_length, embedding, the_node, r):
@@ -76,6 +76,7 @@ def fastmap_L2(G, K, epsilon):
         raw_length_b = nx.single_source_dijkstra_path_length(NG, node_b)
         length_b = adjust_length(raw_length_b, embedding, node_b, r)
         dis_ab = length_a[node_b]
+        print("Iteration {}, largest distance: {}".format(r+1, dis_ab))
         if dis_ab < epsilon:
             break
         # Calcute the embedding
@@ -83,7 +84,6 @@ def fastmap_L2(G, K, epsilon):
             p_ir = float(length_a[node]+dis_ab-length_b[node])/(2*math.sqrt(dis_ab))
             embedding[node].append(p_ir)
 
-    print("The final dimension of embedding: {}".format(r+1))
     return embedding
 
 def store_undirected_distances(G, dis_store, pnode, raw_length):
@@ -116,6 +116,7 @@ def fastmap_L1_store(G, K, epsilon, dis_store, PNumber):
             store_undirected_distances(G, dis_store, node_a, nx.single_source_dijkstra_path_length(G, node_a))
             store_undirected_distances(G, dis_store, node_b, nx.single_source_dijkstra_path_length(G, node_b))
         dis_ab = length_a[node_b]
+        print("Iteration {}, largest distance: {}".format(r+1, dis_ab))
         if dis_ab < epsilon:
             break
         # Calcute the embedding
@@ -128,7 +129,6 @@ def fastmap_L1_store(G, K, epsilon, dis_store, PNumber):
             NG[i][j]['weight'] = w - abs(embedding[i][r]-embedding[j][r])
             if abs(NG[i][j]['weight'])<0.00001:
                 NG[i][j]['weight'] = 0
-    print("The final dimension of embedding: {}".format(r+1))
     return embedding
 
 def fastmap_L2_store(G, K, epsilon, dis_store, PNumber):
@@ -158,12 +158,60 @@ def fastmap_L2_store(G, K, epsilon, dis_store, PNumber):
             store_undirected_distances(G, dis_store, node_a, raw_length_a)
             store_undirected_distances(G, dis_store, node_b, raw_length_b)
         dis_ab = length_a[node_b]
+        print("Iteration {}, largest distance: {}".format(r+1, dis_ab))
         if dis_ab < epsilon:
             break
         # Calcute the embedding
         for node in list(NG.nodes()):
             p_ir = float(length_a[node]+dis_ab-length_b[node])/(2*math.sqrt(dis_ab))
             embedding[node].append(p_ir)
+    return embedding
 
-    print("The final dimension of embedding: {}".format(r+1))
+def adjust_length_l1(raw_length, embedding, the_node, r):
+    nodes = list(embedding.keys())
+    length = {}
+    for node in nodes:
+        length[node] = raw_length[node]
+        i = 0
+        while i < r:
+            length[node] -= abs(embedding[node][i]-embedding[the_node][i])
+            if (length[node])<0.00001:
+                length[node] = 0
+            i = i+1
+    return length
+
+def fastmap_L1_store_noupgrade(G, K, epsilon, dis_store, PNumber):
+    NG = G.copy()
+    embedding = {}
+    # initial the embedding as a dict
+    for node in list(NG.nodes()):
+        embedding[node]=[]
+    for r in range(K):
+        node_a = choice(list(NG.nodes()))
+        node_b = node_a
+        # Find the farthest nodes a, b
+        for t in range(C):
+            raw_length = nx.single_source_dijkstra_path_length(NG, node_a)
+            length = adjust_length_l1(raw_length, embedding, node_a, r)
+            node_c = max(length.items(), key=operator.itemgetter(1))[0]
+            if node_c == node_b:
+                break
+            else:
+                node_b = node_a
+                node_a = node_c
+        raw_length_a = nx.single_source_dijkstra_path_length(NG, node_a)
+        length_a = adjust_length_l1(raw_length_a, embedding, node_a, r)
+        raw_length_b = nx.single_source_dijkstra_path_length(NG, node_b)
+        length_b = adjust_length_l1(raw_length_b, embedding, node_b, r)
+        if r < PNumber:
+            store_undirected_distances(G, dis_store, node_a, nx.single_source_dijkstra_path_length(G, node_a))
+            store_undirected_distances(G, dis_store, node_b, nx.single_source_dijkstra_path_length(G, node_b))
+        dis_ab = length_a[node_b]
+        print("Iteration {}, largest distance: {}".format(r+1, dis_ab))
+        if dis_ab < epsilon:
+            break
+        # Calcute the embedding
+        for node in list(NG.nodes()):
+            p_ir = float(length_a[node]+dis_ab-length_b[node])/2
+            embedding[node].append(p_ir)
     return embedding
